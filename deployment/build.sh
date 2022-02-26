470b4294d446a49aea61671ba5f41db2aa479ecb
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-02-27 02:30:25 +0530

    Initial

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build -t whr-wc-image:prd -f Dockerfile .
+docker build -t wrh-wc-image:prd -f Dockerfile .
 if [ `echo $?` == 0 ]; then
-	docker rm -f whr-wc
-	docker run -dt --restart=always -p 8004:8004 --name whr-wc whr-wc-image:prd
+	docker rm -f wrh-wc
+	docker run -dt --restart=always -p 8004:8004 --name wrh-wc wrh-wc-image:prd
 fi
